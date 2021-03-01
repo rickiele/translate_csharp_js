ec9668939e87215bc09a35cf4f4a36a8c3820e10
Rickie Le <rickienthefishies@gmail.com>
2021-03-01 09:29:11 -0600

    finished exercise

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,22 @@
 // Put your code here
+
+// Array with strings
+let names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"];
+
+// Log in console
+console.log(names);
+
+// Iterate over all the names
+for (let name of names) {
+    console.log(names);
+};
+
+// Filter the array to create a new array that only has
+// place names that start with the word "The"
+let theNames = names.filter((n) => n.startsWith("The"));
+
+console.log("The Place Names");
+for (let name of theNames) {
+    console.log(name);
+}
