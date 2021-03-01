0d58b2d3b6252e6885446a95a0cbce28ae3dd3f8
Rickie Le <rickienthefishies@gmail.com>
2021-03-01 11:04:12 -0600

    gets random value of 2 rolled die

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,53 @@
 // Put your code here
+
+console.log("Let's roll some dice, baby!");
+console.log("---------------------------");
+
+// Function to roll dice
+const Roll = (min, max) => {
+    min = Math.ceil(1);
+    max = Math.floor(6);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// For loop to show 10 rolls
+for (let i = 0; i < 10; i++) {
+    let die1 = Roll();
+    let die2 = Roll();
+
+    let message = `${die1} + ${die2} === ${die1 + die2}`;
+
+    if (die1 === die2) {
+        message += " DOUBLES!";
+    }
+
+    console.log(message)
+}
+
+// Switch Statement for die emojis
+const what = () => {
+    let dieString = "Unknown";
+
+    switch (this.Value) {
+        case 1:
+            dieString = "\u2680";
+            break;
+        case 2:
+            dieString = "\u2681";
+            break;
+        case 3:
+            dieString = "\u2682";
+            break;
+        case 4:
+            dieString = "\u2683";
+            break;
+        case 5:
+            dieString = "\u2684";
+            break;
+        case 6:
+            dieString = "\u2685";
+            break;
+    }
+
+    return dieString;
+}
